7ca18a5e07be0d322a68e56440dc20e658ccfdb7
PhaNguyen24 <147313916+PhaNguyen24@users.noreply.github.com>
2023-10-16 18:17:26 +0700

    Update Day 11_baitap.sql

--- Day 11_baitap.sql
+++ Day 11_baitap.sql
@@ -15,4 +15,20 @@ LEFT JOIN texts AS t ON e.email_id = t.email_id
 WHERE signup_action IS NOT NULL;
 
 --BÀI 3
+SELECT b.age_bucket,
+ROUND(100.0*SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent 
+ELSE 0 END)/SUM(a.time_spent),2) as send_perc,
+ROUND(100*SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent
+ELSE 0 END)/SUM(a.time_spent),2) as open_per
+FROM 
+activities a JOIN age_breakdown b ON a.user_id=b.user_id
+WHERE a.activity_type IN ('send','open')
+GROUP BY b.age_bucket;
 
+--BÀI 4
+SELECT *
+--Customer_id,
+FROM customer_contracts as a
+JOIN products as b ON a.product_id	=b.product_id	
+WHERE product_category in (Analytics,Containers,Compute)
+--GROUP BY customer_id	;
